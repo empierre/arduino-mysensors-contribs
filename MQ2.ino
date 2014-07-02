ffc3ccb3b91438c5e50426801114f79851508084
epierre <epierre@e-nef.com>
2014-07-02 22:44:18 +0200

    updated, compiled...

--- MQ2.ino
+++ MQ2.ino
@@ -22,11 +22,24 @@
 #include <Sensor.h>  
 #include <Wire.h> 
 
-#define 		CHILD_ID_MQ 0
-#define 		MQ2_SENSOR_ANALOG_PIN 0
+#define 	CHILD_ID_MQ                   0 
+/************************Hardware Related Macros************************************/
+#define 	MQ_SENSOR_ANALOG_PIN         (0)  //define which analog input channel you are going to use
+#define         RL_VALUE                     (5)     //define the load resistance on the board, in kilo ohms
+#define         RO_CLEAN_AIR_FACTOR          (9.83)  //RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
+                                                     //which is derived from the chart in datasheet
+/***********************Software Related Macros************************************/
+#define         CALIBARAION_SAMPLE_TIMES     (50)    //define how many samples you are going to take in the calibration phase
+#define         CALIBRATION_SAMPLE_INTERVAL  (500)   //define the time interal(in milisecond) between each samples in the
+                                                     //cablibration phase
+#define         READ_SAMPLE_INTERVAL         (50)    //define how many samples you are going to take in normal operation
+#define         READ_SAMPLE_TIMES            (5)     //define the time interal(in milisecond) between each samples in 
+                                                     //normal operation
+/**********************Application Related Macros**********************************/
 #define         GAS_LPG                      (0)
 #define         GAS_CO                       (1)
 #define         GAS_SMOKE                    (2)
+/*****************************Globals***********************************************/
 unsigned long SLEEP_TIME = 30; // Sleep time between reads (in seconds)
 //VARIABLES
 float Ro = 10000.0;    // this has to be tuned 10K Ohm
@@ -60,13 +73,14 @@ void setup()
   // Register all sensors to gateway (they will be created as child devices)
   gw.sendSensorPresentation(CHILD_ID_MQ, 22);  
 
-  Ro = MQCalibration(MQ2_SENSOR_ANALOG_PIN);         //Calibrating the sensor. Please make sure the sensor is in clean air 
+  Ro = MQCalibration(MQ_SENSOR_ANALOG_PIN);         //Calibrating the sensor. Please make sure the sensor is in clean air 
                                                     //when you perform the calibration  
+  Serial.println(Ro);                                                    
 }
 
 void loop()      
 {     
-  uint16_t valMQ = MQGetGasPercentage(MQRead(MQ_SENSOR_ANALOG_PIN)/Ro,GAS_CO)
+  uint16_t valMQ = MQGetGasPercentage(MQRead(MQ_SENSOR_ANALOG_PIN)/Ro,GAS_CO);
   Serial.println(val);
   
    Serial.print("LPG:"); 
@@ -186,3 +200,4 @@ Remarks: By using the slope and a point of the line. The x(logarithmic value of 
 int  MQGetPercentage(float rs_ro_ratio, float *pcurve)
 {
   return (pow(10,( ((log(rs_ro_ratio)-pcurve[1])/pcurve[2]) + pcurve[0])));
+}
