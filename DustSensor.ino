8ad27ba611238c5c4f4119ec0fd544b9b8ae9e12
epierre <epierre@e-nef.com>
2014-08-02 12:22:28 +0200

    initial version

---
+++ DustSensor.ino
@@ -0,0 +1,84 @@
+/*
+  Vera Arduino Dust Sensort
+
+  connect the sensor as follows :
+
+  VCC       >>> 5V
+  A         >>> A0
+  GND       >>> GND
+
+  BAsed on: http://www.dfrobot.com/wiki/index.php/Sharp_GP2Y1010AU 
+  Contribution: epierre
+ 
+*/
+
+#include <Sleep_n0m1.h>
+#include <SPI.h>
+#include <RF24.h>
+#include <EEPROM.h>  
+#include <Sensor.h>  
+#include <Wire.h>
+
+#define CHILD_ID_DUST 0
+#define DUST_SENSOR_ANALOG_PIN 0
+
+unsigned long SLEEP_TIME = 30; // Sleep time between reads (in seconds)
+//VARIABLES
+int val = 0;           // variable to store the value coming from the sensor
+float valDUST =0.0;
+float lastDUST =0.0;
+int samplingTime = 280;
+int deltaTime = 40;
+int sleepTime = 9680;
+float voMeasured = 0;
+float calcVoltage = 0;
+float dustDensity = 0;
+
+Sensor gw;
+Sleep sleep;
+
+void setup()  
+{
+  gw.begin();
+
+  // Send the sketch version information to the gateway and Controller
+  gw.sendSketchInfo("Dust Sensor", "1.0");
+
+  // Register all sensors to gateway (they will be created as child devices)
+  gw.sendSensorPresentation(CHILD_ID_DUST, 23);  
+   
+}
+
+void loop()      
+{    
+  uint16_t voMeasured = analogRead(DUST_SENSOR_ANALOG_PIN);// Get DUST value
+
+  // 0 - 5V mapped to 0 - 1023 integer values
+  // recover voltage
+  calcVoltage = voMeasured * (5.0 / 1024.0);
+
+  // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
+  // Chris Nafis (c) 2012
+  dustDensity = 0.17 * calcVoltage - 0.1;
+ 
+  Serial.print("Raw Signal Value (0-1023): ");
+  Serial.print(voMeasured);
+  
+  Serial.print(" - Voltage: ");
+  Serial.print(calcVoltage);
+  
+  Serial.print(" - Dust Density: ");
+  Serial.println(dustDensity); // unit: mg/m3
+ 
+  if (ceil(dustDensity) != lastDUST) {
+      gw.sendVariable(CHILD_ID_DUST, V_VAR1, (int)ceil(dustDensity));
+      lastDUST = ceil(dustDensity);
+  }
+ 
+  // Power down the radio.  Note that the radio will get powered back up
+  // on the next write() call.
+  delay(1000); //delay to allow serial to fully print before sleep
+  gw.powerDown();
+  sleep.pwrDownMode(); //set sleep mode
+  sleep.sleepDelay(SLEEP_TIME * 1000); //sleep for: sleepTime
+}
