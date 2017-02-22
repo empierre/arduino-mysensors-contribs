44979511db8c90bc21e8e7c472eaac9ad3f2fa36
Emmanuel <epierre@e-nef.com>
2017-02-22 13:51:06 +0100

    Update DustSensor_Sharp_GP2Y1010AU.ino
    close #20

--- DustSensor_Sharp_GP2Y1010AU.ino
+++ DustSensor_Sharp_GP2Y1010AU.ino
@@ -1,41 +1,57 @@
-/*
-  Arduino Dust Sensor for Sharp_GP2Y1010AU
-
-  connect the sensor as follows :
-
-  *Please note both analogic and digital must be wired at the same time*
-
-  Analogic connector: 
-    VCC       >>> 5V
-    A         >>> A1
-    GND       >>> GND
-    
-  Digital connector: 
-    VCC       >>> 5V
-    D         >>> D1
-    GND       >>> GND
-
-  Based on: http://www.dfrobot.com/wiki/index.php/Sharp_GP2Y1010AU 
-  Authors: Cyrille Médard de Chardon (serialC), Christophe Trefois (Trefex)
-  Contribution: epierre
-  COnverted to 1.4 by Henrik Ekblad
-  
-  The dust sensor used (see purchase guide for latest link):
-  http://rover.ebay.com/rover/1/711-53200-19255-0/1?icep_ff3=2&pub=5575069610&toolid=10001&campid=5337433187&customid=&icep_item=171259125886&ipn=psmain&icep_vectorid=229466&kwid=902099&mtid=824&kw=lg
-  
+/**
+* The MySensors Arduino library handles the wireless radio link and protocol
+* between your home built sensors/actuators and HA controller of choice.
+* The sensors forms a self healing radio network with optional repeaters. Each
+* repeater and gateway builds a routing tables in EEPROM which keeps track of the
+* network topology allowing messages to be routed to nodes.
+*
+* Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+* Copyright (C) 2013-2015 Sensnology AB
+* Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+*
+* Documentation: http://www.mysensors.org
+* Support Forum: http://forum.mysensors.org
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* version 2 as published by the Free Software Foundation.
+*
+*******************************
+*
+* REVISION HISTORY
+* Version 1.0 - epierre
+* Converted to 1.4 by Henrik Ekblad
+* corrections by brnwshr
+* 
+* DESCRIPTION
+* Arduino Dust Sensort
+*
+* connect the sensor as follows :
+* 
+*   VCC       >>> 5V
+*   A         >>> A0
+*   GND       >>> GND
+*   LED       >>> 2
+*
+* Based on: http://www.dfrobot.com/wiki/index.php/Sharp_GP2Y1010AU 
+* Authors: Cyrille Médard de Chardon (serialC), Christophe Trefois (Trefex)
+* 
+* http://www.mysensors.org/build/dust
+* 
 */
 
 #include <MySensor.h>  
 #include <SPI.h>
-
 #define CHILD_ID_DUST 0
-#define DUST_SENSOR_ANALOG_PIN 1
-#define DUST_SENSOR_DIGITAL_PIN 1
+#define DUST_SENSOR_ANALOG_PIN 0
 
-unsigned long SLEEP_TIME = 30*1000; // Sleep time between reads (in milliseconds)
+int ledPower = 2;
+const int numreadings = 60;
+
+unsigned long SLEEP_TIME = 10*1000; // Sleep time between reads (in milliseconds)
 //VARIABLES
-int val = 0;           // variable to store the value coming from the sensor
-float valDUST =0.0;
+int val = 0;// variable to store the value coming from the sensor
+float valDUST = 0.0;
 float lastDUST =0.0;
 int samplingTime = 280;
 int deltaTime = 40;
@@ -47,49 +63,50 @@ float dustDensity = 0;
 MySensor gw;
 MyMessage dustMsg(CHILD_ID_DUST, V_LEVEL);
 
-void setup()  
-{
-  gw.begin();
-
-  // Send the sketch version information to the gateway and Controller
-  gw.sendSketchInfo("Dust Sensor", "1.2");
-
-  // Register all sensors to gateway (they will be created as child devices)
-  gw.present(CHILD_ID_DUST, S_DUST);  
-  
-  pinMode(DUST_SENSOR_DIGITAL_PIN,OUTPUT); //light on dust sensor led
-   
+void setup(){
+ gw.begin();
+ pinMode(ledPower,OUTPUT);
+ // Send the sketch version information to the gateway and Controller
+ gw.sendSketchInfo("Dust Sensor", "1.1");
+ // Register all sensors to gateway (they will be created as child devices)
+ gw.present(CHILD_ID_DUST, S_DUST);  
 }
 
-void loop()      
-{    
-  digitalWrite(DUST_SENSOR_DIGITAL_PIN,LOW); // power on the LED
-  delayMicroseconds(280);
-  uint16_t voMeasured = analogRead(DUST_SENSOR_ANALOG_PIN);// Get DUST value
-  delayMicroseconds(40);
-  digitalWrite(DUST_SENSOR_DIGITAL_PIN,HIGH); // turn the LED off
-  
-  // 0 - 5V mapped to 0 - 1023 integer values
-  // recover voltage
-  calcVoltage = voMeasured * (5.0 / 1024.0);
+void loop(){
+float cumsum = 0;
+float temp = 0;
+float temp1 = 0;
+float cum_density = 0;
+     for (int sample = 0; sample < numreadings; sample ++){ // loop reading dust sensor
+       digitalWrite(ledPower,LOW); // power on the LED
+       delayMicroseconds(samplingTime);
+       uint16_t voMeasured = analogRead(DUST_SENSOR_ANALOG_PIN);// Get DUST value
+       delayMicroseconds(deltaTime);
+        digitalWrite(ledPower,HIGH); // turn the LED off
+       // 0 - 5V mapped to 0 - 1023 integer values
+       // recover voltage
+       temp = voMeasured * (5.0 / 1024.0);
+       cumsum = cumsum + temp;// cumulative sum over 60 seconds
+       delay(1000);
+       Serial.print("reading sample: ");
+       Serial.println(sample);
+       Serial.print("Raw Signal Value (0-1023): ");
+       Serial.println(voMeasured);    
+       Serial.print(" - cumulative: ");
+       Serial.println(cumsum);
+}
+// linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
+// Chris Nafis (c) 2012
+dustDensity = (0.17 * cumsum - 0.1)* (1000/60);
+       
+Serial.print(" - Dust Density: ");
+Serial.println(dustDensity); // unit: ug/m3
+Serial.println("#########################################");
 
-  // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
-  // Chris Nafis (c) 2012
-  dustDensity = (0.17 * calcVoltage - 0.1)*1000;
- 
-  Serial.print("Raw Signal Value (0-1023): ");
-  Serial.print(voMeasured);
-  
-  Serial.print(" - Voltage: ");
-  Serial.print(calcVoltage);
-  
-  Serial.print(" - Dust Density: ");
-  Serial.println(dustDensity); // unit: ug/m3
- 
-  if (ceil(dustDensity) != lastDUST) {
-      gw.send(dustMsg.set((int)ceil(dustDensity)));
-      lastDUST = ceil(dustDensity);
-  }
- 
-  gw.sleep(SLEEP_TIME);
+//  if (ceil(dustDensity) != lastDUST) {
+//      gw.send(dustMsg.set((int)ceil(dustDensity)));
+//      lastDUST = ceil(dustDensity);
+//  }
+gw.send(dustMsg.set((int)ceil(dustDensity)));
+ gw.sleep(SLEEP_TIME);
 }
